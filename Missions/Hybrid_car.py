36ea080dc38d88a876aaf8e19cba452b0fd4d49e
Rao <103417697+Aby-ss@users.noreply.github.com>
2023-02-07 12:30:38 +0400

    Added SPIKE code

--- Missions/Hybrid_car.py
+++ Missions/Hybrid_car.py
@@ -1,7 +1,5 @@
-from pybricks.pupdevices import Motor
-from pybricks.parameters import Port
-from pybricks.tools import wait
+from spike import Motor
+from spike import MotorPair
 
-Movement_Motor_1 = Motor(Port.F)
-Movement_Motor_2 = Motor(Port.B)
+movement_pair = MotorPair('F', 'B')
 
--- Missions/Toy_Factory.py
+++ Missions/Toy_Factory.py
@@ -1,3 +1,11 @@
-from pybricks.pupdevices import Motor
-from pybricks.parameters import Port
-from pybricks.tools import wait
+from spike import Motor
+from spike import MotorPair
+
+movement_pair = MotorPair('F', 'B')
+movement_pair.set_default_speed(25)
+movement_pair.move(1.6, 'rotations')
+
+D_motor = Motor('D')
+D_motor.set_default_speed(20)
+D_motor.run_for_seconds(0.6, -75)
+D_motor.move(-2, 'rotations')
