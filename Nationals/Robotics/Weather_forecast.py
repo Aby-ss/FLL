e343d6a4c562149d2f4b582b31311f5ba720db0c
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-06-13 07:37:00 +0400

    Updated variables ✅

--- Nationals/Robotics/Weather_forecast.py
+++ Nationals/Robotics/Weather_forecast.py
@@ -21,17 +21,31 @@ forecast = forecast_data["data"]["forecast"]
 
 for entry in forecast:
     print(f"Time: {entry['time']}")
+    time = ['time']
     print(f"Dew Point: {entry['dewPoint']}")
+    dew_point = ['dewPoint']
     print(f"Temperature: {entry['temperature']}")
+    temperature = ['temperation']
     print(f"Apparent Temperature: {entry['apparentTemperature']}")
+    apparent_temperature = ['apparentTemperature']
     print(f"Humidity: {entry['humidity']}")
+    humidity = ['humidity']
     print(f"Pressure: {entry['pressure']}")
+    pressure = ['pressure']
     print(f"Wind Speed: {entry['windSpeed']}")
+    wind_speed = ['windSpeed']
     print(f"Wind Bearing: {entry['windBearing']}")
+    wind_bearing = ['windBearing']
     print(f"Wind Gust: {entry['windGust']}")
+    wind_gust = ['windGust']
     print(f"Cloud Cover: {entry['cloudCover']}")
+    cloud_cover = ['cloudCover']
     print(f"Visibility: {entry['visibility']}")
+    visibility = ['visibility']
     print(f"UV Index: {entry['uvIndex']}")
+    UV_indox = ['uvIndex']
     print(f"Summary: {entry['summary']}")
+    summary = ['summary']
     print(f"Icon: {entry['icon']}")
+    icon = ['icon']
     print()
