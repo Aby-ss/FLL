1891c7d89b8c7c5053d653e4352da8f08d3e0c6d
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-06-19 12:46:50 +0400

    Added Separate variables from weather details ✅

--- Nationals/Innovation/Weather_forecast.py
+++ Nationals/Innovation/Weather_forecast.py
@@ -23,17 +23,31 @@ forecast = forecast_data["data"]["forecast"]
 print("Forecast Data:")
 entry = forecast[0]
 print(f"Time: {entry['time']}")
+time = entry['time']
 print(f"Dew Point: {entry['dewPoint']}")
+dewPoint = entry['dewPoint']
 print(f"Temperature: {entry['temperature']}")
+temp = entry['temperature']
 print(f"Apparent Temperature: {entry['apparentTemperature']}")
+app_temp = entry['apparentTemperature']
 print(f"Humidity: {entry['humidity']}")
+humidity = entry['humidity']
 print(f"Pressure: {entry['pressure']}")
+pressure = entry['pressure']
 print(f"Wind Speed: {entry['windSpeed']}")
+wind_speed = entry['windSpeed']
 print(f"Wind Bearing: {entry['windBearing']}")
+Wind_bearing = entry['windBearing']
 print(f"Wind Gust: {entry['windGust']}")
+wind_Gust = entry['windGust']
 print(f"Cloud Cover: {entry['cloudCover']}")
+cloud_Cover = entry['cloudCover']
 print(f"Visibility: {entry['visibility']}")
+visibility = entry['visibility']
 print(f"UV Index: {entry['uvIndex']}")
+uv_Index = entry['uvIndex']
 print(f"Summary: {entry['summary']}")
+summary = entry['summary']
 print(f"Icon: {entry['icon']}")
+weather_icon = entry['icon']
 print()
