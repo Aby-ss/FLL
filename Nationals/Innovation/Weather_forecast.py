43238e8c7f897c2c93e8b4237eb7d04a0ef2a6ff
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-06-19 16:34:16 +0400

    Fixed error with Weather forecasting API ✅ + Weather + Energy analysis check ✅

--- Nationals/Innovation/Weather_forecast.py
+++ Nationals/Innovation/Weather_forecast.py
@@ -5,7 +5,7 @@ from rich.pretty import pprint
 conn = http.client.HTTPSConnection("api.ambeedata.com")
 
 headers = {
-    'x-api-key': "7eafc9fb1393d7456d0778e8bde486f02bcd494f87792146729bf4e61062ae36",
+    'x-api-key': "0f224f1547e7f994559ef153134453aecf2a16904e9fbd9bf111fe4ed8434dad",
     'Content-type': "application/json"
 }
 
@@ -20,34 +20,34 @@ forecast_data = json.loads(json_data)
 forecast = forecast_data["data"]["forecast"]
 
 # Print the first entry of forecast data
-print("Forecast Data:")
+#print("Forecast Data:")
 entry = forecast[0]
-print(f"Time: {entry['time']}")
+#print(f"Time: {entry['time']}")
 time = entry['time']
-print(f"Dew Point: {entry['dewPoint']}")
+#print(f"Dew Point: {entry['dewPoint']}")
 dewPoint = entry['dewPoint']
-print(f"Temperature: {entry['temperature']}")
+#print(f"Temperature: {entry['temperature']}")
 temp = entry['temperature']
-print(f"Apparent Temperature: {entry['apparentTemperature']}")
+#print(f"Apparent Temperature: {entry['apparentTemperature']}")
 app_temp = entry['apparentTemperature']
-print(f"Humidity: {entry['humidity']}")
+#print(f"Humidity: {entry['humidity']}")
 humidity = entry['humidity']
-print(f"Pressure: {entry['pressure']}")
+#print(f"Pressure: {entry['pressure']}")
 pressure = entry['pressure']
-print(f"Wind Speed: {entry['windSpeed']}")
+#print(f"Wind Speed: {entry['windSpeed']}")
 wind_speed = entry['windSpeed']
-print(f"Wind Bearing: {entry['windBearing']}")
+#print(f"Wind Bearing: {entry['windBearing']}")
 Wind_bearing = entry['windBearing']
-print(f"Wind Gust: {entry['windGust']}")
+#print(f"Wind Gust: {entry['windGust']}")
 wind_Gust = entry['windGust']
-print(f"Cloud Cover: {entry['cloudCover']}")
+#print(f"Cloud Cover: {entry['cloudCover']}")
 cloud_Cover = entry['cloudCover']
-print(f"Visibility: {entry['visibility']}")
+#print(f"Visibility: {entry['visibility']}")
 visibility = entry['visibility']
-print(f"UV Index: {entry['uvIndex']}")
+#print(f"UV Index: {entry['uvIndex']}")
 uv_Index = entry['uvIndex']
-print(f"Summary: {entry['summary']}")
+#print(f"Summary: {entry['summary']}")
 summary = entry['summary']
-print(f"Icon: {entry['icon']}")
+#print(f"Icon: {entry['icon']}")
 weather_icon = entry['icon']
-print()
+#print()
